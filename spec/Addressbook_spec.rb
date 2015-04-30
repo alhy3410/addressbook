03d386f666fc18612e3e25a74ca47a34863210e3
andrew lee & chase reckling <student@epicodus.com>
2015-04-30 10:18:30 -0700

    update method to use brackets

--- spec/Addressbook_spec.rb
+++ spec/Addressbook_spec.rb
@@ -12,35 +12,35 @@ describe('Contact') do
 
   describe('#first_name') do
     it("will add the first name of the contact infomation") do
-      new_contact = Contact.new({:first_name => "John",:last_name => "Smith", :birth_month => "July"})
+      new_contact = Contact.new({:first_name => "John"})
       expect(new_contact.first_name()).to(eq("John"))
     end
   end
 
   describe('#last_name') do
     it("will add the last name of the contact information") do
-      new_contact = Contact.new({:first_name => "John",:last_name => "Smith", :birth_month => "July"})
+      new_contact = Contact.new({:last_name => "Smith"})
       expect(new_contact.last_name()).to(eq("Smith"))
     end
   end
 
   describe('#birth_month') do
     it("will add the birth month of the contact") do
-      new_contact = Contact.new({:first_name => "John", :last_name => "Smith", :birth_month => "July"})
+      new_contact = Contact.new({:birth_month => "July"})
       expect(new_contact.birth_month()).to(eq("July"))
     end
   end
 
   describe('#home_phone') do
     it("return contact home phone number") do
-      new_phones = Phone.new({:home_phone => "555-555-5555", :work_phone => "555-555-5556"})
+      new_phones = Phone.new({:home_phone => "555-555-5555"})
       expect(new_phones.home_phone()).to(eq("555-555-5555"))
     end
   end
 
   describe('#work_phone') do
     it("return contact work phone number") do
-      new_phones = Phone.new({:home_phone => "555-555-5555", :work_phone => "555-555-5556"})
+      new_phones = Phone.new({:work_phone => "555-555-5556"})
       expect(new_phones.work_phone()).to(eq("555-555-5556"))
     end
   end
