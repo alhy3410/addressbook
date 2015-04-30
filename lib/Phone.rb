427c18bad83dbe92d1a4c5734cf0c7db1d465b77
andrew lee & chase reckling <student@epicodus.com>
2015-04-30 10:25:05 -0700

    add cell phone to Phone class, spec pass

--- lib/Phone.rb
+++ lib/Phone.rb
@@ -1,10 +1,21 @@
 class Phone
-  @@allphones = []
+  @@all_phones = []
   attr_reader(:home_phone, :work_phone, :cell_phone)
 
   define_method(:initialize) do |attributes|
     @home_phone = attributes[:home_phone]
     @work_phone = attributes[:work_phone]
     @cell_phone = attributes[:cell_phone]
+  end
+  define_singleton_method(:clear) do
+    @@all_phones = []
+  end
+
+  define_singleton_method(:all) do
+    @@all_phones
+  end
+
+  define_method(:save) do
+    @@all_phones.push(self)
   end
 end
--- spec/Addressbook_spec.rb
+++ spec/Addressbook_spec.rb
@@ -3,49 +3,50 @@ require('Addressbook')
 require('Contact')
 require('Phone')
 
-describe('Contact') do
+describe('Addressbook') do
 
   before() do
     Contact.clear()
+    Phone.clear()
   end
 
 
-  describe('#first_name') do
+  describe('Contact#first_name') do
     it("will add the first name of the contact infomation") do
       new_contact = Contact.new({:first_name => "John"})
       expect(new_contact.first_name()).to(eq("John"))
     end
   end
 
-  describe('#last_name') do
+  describe('Contact#last_name') do
     it("will add the last name of the contact information") do
       new_contact = Contact.new({:last_name => "Smith"})
       expect(new_contact.last_name()).to(eq("Smith"))
     end
   end
 
-  describe('#birth_month') do
+  describe('Contact#birth_month') do
     it("will add the birth month of the contact") do
       new_contact = Contact.new({:birth_month => "July"})
       expect(new_contact.birth_month()).to(eq("July"))
     end
   end
 
-  describe('#home_phone') do
+  describe('Phone#home_phone') do
     it("return contact home phone number") do
       new_phones = Phone.new({:home_phone => "555-555-5555"})
       expect(new_phones.home_phone()).to(eq("555-555-5555"))
     end
   end
 
-  describe('#work_phone') do
+  describe('Phone#work_phone') do
     it("return contact work phone number") do
       new_phones = Phone.new({:work_phone => "555-555-5556"})
       expect(new_phones.work_phone()).to(eq("555-555-5556"))
     end
   end
 
-  describe('#cell_phone') do
+  describe('Phone#cell_phone') do
     it("return contact cell phone number") do
       new_phones = Phone.new({:cell_phone => "555-555-5557"})
       expect(new_phones.cell_phone()).to(eq("555-555-5557"))
@@ -54,7 +55,7 @@ describe('Contact') do
 
 
 
-  describe('.clear') do
+  describe('Contact.clear') do
     it("will clear all contacts from the list") do
       Contact.new({:first_name => "John", :last_name => "Smith", :birth_month => "July"}).save()
       Contact.clear()
@@ -62,11 +63,19 @@ describe('Contact') do
     end
   end
 
-  describe('.all') do
+  describe('Contact.all') do
     it("will show all contacts") do
       new_contact = Contact.new({:first_name => "John", :last_name => "Smith", :birth_month => "July"})
       new_contact.save()
       expect(Contact.all()).to(eq([new_contact]))
+    end
+  end
+
+  describe('Phone.clear') do
+    it("will clear all phones from the list") do
+      Phone.new({:home_phone => "555-555-5555"}).save()
+      Phone.clear()
+      expect(Phone.all()).to(eq([]))
     end
   end
 
