887d8ba1e1340d4081819fa63da4a0e5f9fb7fe8
andrew lee & chase reckling <student@epicodus.com>
2015-04-30 09:49:01 -0700

    adds clear, save, birthmonth, all, last_name functions to the Contact Class

--- lib/Contact.rb
+++ lib/Contact.rb
@@ -1,9 +1,22 @@
 class Contact
   @@all_contacts = []
-  attr_reader(:first_name)
+  attr_reader(:first_name, :last_name, :birth_month)
 
   define_method(:initialize) do |attributes|
     @first_name = attributes.fetch(:first_name)
+    @last_name = attributes.fetch(:last_name)
+    @birth_month = attributes.fetch(:birth_month)
   end
 
+  define_singleton_method(:clear) do
+    @@all_contacts = []
+  end
+
+  define_singleton_method(:all) do
+    @@all_contacts
+  end
+
+  define_method(:save) do
+    @@all_contacts.push(self)
+  end
 end
--- spec/Addressbook_spec.rb
+++ spec/Addressbook_spec.rb
@@ -3,10 +3,47 @@ require('Addressbook')
 require('Contact')
 
 describe('Contact') do
+
+  before() do
+    Contact.clear()
+  end
+
+
   describe('#first_name') do
     it("will add the first name of the contact infomation") do
-      new_contact = Contact.new({:first_name => "John"})
+      new_contact = Contact.new({:first_name => "John",:last_name => "Smith", :birth_month => "July"})
       expect(new_contact.first_name()).to(eq("John"))
     end
   end
+
+  describe('#last_name') do
+    it("will add the last name of the contact information") do
+      new_contact = Contact.new({:first_name => "John",:last_name => "Smith", :birth_month => "July"})
+      expect(new_contact.last_name()).to(eq("Smith"))
+    end
+  end
+
+  describe('#birth_month') do
+    it("will add the birth month of the contact") do
+      new_contact = Contact.new({:first_name => "John", :last_name => "Smith", :birth_month => "July"})
+      expect(new_contact.birth_month()).to(eq("July"))
+    end
+  end
+
+  describe('.clear') do
+    it("will clear all contacts from the list") do
+      Contact.new({:first_name => "John", :last_name => "Smith", :birth_month => "July"}).save()
+      Contact.clear()
+      expect(Contact.all()).to(eq([]))
+    end
+  end
+
+  describe('.all') do
+    it("will show all contacts") do
+      new_contact = Contact.new({:first_name => "John", :last_name => "Smith", :birth_month => "July"})
+      new_contact.save()
+      expect(Contact.all()).to(eq([new_contact]))
+    end
+  end
+
 end
