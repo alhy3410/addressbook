763fcee20876c2f2dfe0ea3066de0472521f0f56
andrew lee & chase reckling <student@epicodus.com>
2015-04-30 11:28:45 -0700

    add a new address book and save it to all address books in the Addressbook class

--- lib/Addressbook.rb
+++ lib/Addressbook.rb
@@ -6,4 +6,13 @@ class Addressbook
   define_method(:initialize) do |attributes|
     @name = attributes[:name]
   end
+
+  define_singleton_method(:all) do
+    @@all_addressbooks
+  end
+
+  define_method(:save) do
+    @@all_addressbooks.push(self)
+  end
+
 end
--- spec/Addressbook_spec.rb
+++ spec/Addressbook_spec.rb
@@ -8,4 +8,12 @@ describe('Addressbook') do
       expect(friends_address_book_new.name()).to(eq("Friends Address Book"))
     end
   end
+
+  describe('#save') do
+    it("saves the address book") do
+      friends_address_book_new = Addressbook.new({:name => "Friends Address Book"})
+      friends_address_book_new.save()
+      expect(Addressbook.all()).to(eq([friends_address_book_new]))
+    end
+  end
 end
